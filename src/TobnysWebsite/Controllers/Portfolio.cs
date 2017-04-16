dd6919030072ac4be47584b6586e6092ca994217
tobnys <tobnystrom@gmail.com>
2017-04-16 22:43:26 +0200

    Fixed routing and added animations amongst other minor things.

---
+++ src/TobnysWebsite/Controllers/Portfolio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TobnysWebsite.Controllers
+{
+    public class Portfolio : Controller
+    {
+        [Route("portfolio/tobnys")]
+        public IActionResult tobnys()
+        {
+            return View();
+        }
+
+        [Route("portfolio/mkey")]
+        public IActionResult mkey()
+        {
+            return View();
+        }
+    }
+}
